f8eee52c728486d03d4cf99bd19a5b34a5a0cab9
Dylan K <50585244+dylankainth@users.noreply.github.com>
2022-10-19 07:48:29 +0000

    setup flask

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from flask import Flask, render_template, request, redirect, url_for, send_file,after_this_request
+import os 
+import shutil
+
+app = Flask(__name__)
+
 import argparse
 import os
 from PyPDF2 import PdfFileReader, PdfFileWriter
@@ -17,7 +23,6 @@ def cropPageLeft(page):
 		page.cropBox.upperLeft = (0, height)
 		page.cropBox.upperRight = (width, height)
 		
-
 def cropPageRight(page):
 	width = page.mediaBox.lowerRight[0]
 	height = page.mediaBox.upperLeft[1]
@@ -33,7 +38,6 @@ def cropPageRight(page):
 		page.cropBox.upperLeft = (0, height/2)
 		page.cropBox.upperRight = (width, height/2)
 	
-	
 def splitPDF(document, pagesPerDocument):
 	numberOfPages = document.getNumPages()
 	
@@ -46,8 +50,7 @@ def splitPDF(document, pagesPerDocument):
 		writers[i // pagesPerDocument].addPage(document.getPage(i))
 		
 	return writers
-		
-		
+			
 def splitA3Booklet(document1, document2, pagesPerDocument):
 	numPages = document1.getNumPages()
 	
@@ -94,7 +97,6 @@ def splitA3Booklet(document1, document2, pagesPerDocument):
 			
 	return outputWriter
 	
-	
 def scramble(document, pagesPerDocument, split=False):
 	numberOfPages = document.getNumPages()
 	
@@ -128,9 +130,7 @@ def saveDocuments(documents, filenamePrefix):
 		filePath = os.path.join(directory, filename)
 		
 		with open(filePath, "wb") as output:
-			document.write(output)
-	
-	
+			document.write(output)	
 		
 def unscrambler(filename, pagesPerDocument, isBooklet, split, rearrange):
 	pdf = open(filename, "rb")
@@ -168,16 +168,51 @@ def unscrambler(filename, pagesPerDocument, isBooklet, split, rearrange):
 		saveDocuments(documents, filenamePrefix)
 	else:
 		print("You must select at least one option: -r, -s, or -b.")
-	
-		
-def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("filename")
-	parser.add_argument("numpages", type=int, help="The number of pages per document")
-	parser.add_argument("-b", "--booklet", action="store_true", help="Use this option if the original is an A3 booklet. Source PDF is expected to be in landscape with the middle pages coming first.")
-	parser.add_argument("-s", "--split", action="store_true", help="Use this option if you would like the output to be split into multiple files.")
-	parser.add_argument("-r", "--rearrange", action="store_true", help="Use this option to rearrange the pages.")
 
-	args = parser.parse_args()
-	
-	unscrambler(args.filename, args.numpages, args.booklet, args.split, args.rearrange)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/static/<path:path>')
+def serve(path):
+    return send_from_directory('static', path)
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    nopages = int(request.form.get('nopages'))
+    booklet = bool(request.form.get('booklet'))
+    split = bool(request.form.get('split'))
+    rearrange = bool(request.form.get('rearrange'))
+
+    if uploaded_file.filename != '':
+
+        @after_this_request
+        def remove_file(response):
+            try:
+                os.remove(uploaded_file.filename)
+                if split:
+                    os.remove('output.zip')
+                else:
+                    uploaded_file.filename.replace('.pdf',"_output.pdf")
+                shutil.rmtree(uploaded_file.filename.replace('.pdf',"") + "_output")
+            except Exception as error:
+                app.logger.error("Error removing or closing downloaded file handle", error)
+            return response
+            
+        uploaded_file.save(uploaded_file.filename)
+
+        unscrambler(uploaded_file.filename, nopages , booklet, split, rearrange)
+
+        if split:
+            shutil.make_archive('output', 'zip', uploaded_file.filename.replace('.pdf',"") + "_output")
+            return send_file('output.zip', as_attachment=True)
+        elif (not split):
+            return send_file(uploaded_file.filename.replace('.pdf',"_output.pdf"), as_attachment=True)
+
+        return send_file(uploaded_file.filename, as_attachment=True)
+        
+
+    return redirect(url_for('index'))
+
+Flask.run(app)
